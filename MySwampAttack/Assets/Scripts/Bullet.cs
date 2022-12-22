c8de73e4844521774d164fc039dc0d045483de2d
RedWave <kurilov6dv@gmail.com>
2022-12-22 21:14:37 +0300

    Pistol

---
+++ MySwampAttack/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+    
+    private void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);    
+    }
+}
