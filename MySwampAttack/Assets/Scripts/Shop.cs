20f401531c0a364d307b18ae99a19b83061e5ba7
RedWave <kurilov6dv@gmail.com>
2022-12-27 00:07:37 +0300

    1bilds

--- MySwampAttack/Assets/Scripts/Shop.cs
+++ MySwampAttack/Assets/Scripts/Shop.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Shop : MonoBehaviour
 {
