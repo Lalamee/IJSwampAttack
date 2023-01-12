11f3b302bc46a96e7ffba57e33533d35d20f3f5c
RedWave <kurilov6dv@gmail.com>
2023-01-12 18:20:14 +0300

    Uzi.1.3

--- MySwampAttack/Assets/Scripts/Weapon/Uzi.cs
+++ MySwampAttack/Assets/Scripts/Weapon/Uzi.cs
@@ -8,21 +8,17 @@ public class Uzi : Weapon
     
     public override void Shoot(Transform shootPoint)
     {
-        var firingBurstBullet = FiringBurst(shootPoint);
-
-        StartCoroutine(firingBurstBullet);
+        var firingBurstBullet =  StartCoroutine(FiringBurst(shootPoint));
     }
 
     private IEnumerator FiringBurst(Transform spawnPoint)
     {
-        var _delayForSpawn = new WaitForSeconds(_delay);
+        var delayForSpawn = new WaitForSeconds(_delay);
         
         for (int i = 0; i < _countBullet; i++)
         {
             Instantiate(Bullet, spawnPoint.position, Quaternion.identity);
-            yield return _delayForSpawn;
+            yield return delayForSpawn;
         }
-        
-        StopAllCoroutines();
     }
 }
